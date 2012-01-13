95af570cf23eb37eb809501bfb7b79d06a49074e
Eugene Howe <ehowe@engineyard.com>
2012-01-13 15:16:06 -0500

    Fixed rvm output for all methods other than use

--- libs/rvm.rb
+++ libs/rvm.rb
@@ -1,14 +1,19 @@
 module ArshCommands
   def self.rvm(command)
-    command = command.join(' ')
-    output = %x{bash -c "source #{ENV['rvm_path']}/scripts/rvm; rvm #{command}; env | grep -i rvm"}
-    output.split("\n").select { |l| l[/=/] }.each do |n|
-      var, value = n.split(/=/,2)
-      ENV[var] = value
-    end
+    $stdout.sync = true
+    $stderr.sync = true
+    time = Time.now.to_i
+    command = command.is_a?(Array) ? command.join(' ') : command
     case command
     when /^use /
+      @output = %x{bash -c "source #{ENV['rvm_path']}/scripts/rvm; rvm #{command}; env | grep -i rvm"}
+      @output.split("\n").select { |l| l[/=/] }.each do |n|
+        var, value = n.split(/=/,2)
+        ENV[var] = value
+      end
       puts "\e[32mUsing #{ENV['GEM_HOME']}\e[0m"
-    end
+    else
+      exec %Q{bash -c "source #{ENV['rvm_path']}/scripts/rvm; rvm #{command}"}
+     end
   end
 end
