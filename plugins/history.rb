ca1818132c18ddf45cc409ecba108e754a3876ff
Eugene Howe <ehowe@engineyard.com>
2012-01-18 10:50:53 -0500

    History is now persistent between sessions

--- plugins/history.rb
+++ plugins/history.rb
@@ -1,5 +1,16 @@
 module ArshCommands
   def self.history(args)
-    puts ArshCommands::INTHIST.find_all { |x| puts x }
+    case args.join(" ")
+    when /-v/
+      puts ArshCommands::INTHIST
+    when /-h/
+      puts <<EOF
+Usage:
+  -v: Verbose output; Show Timestamps.
+  -h: Show this message.
+EOF
+    else
+      ArshCommands::INTHIST[0].map { |x| puts x.gsub(/^\[.*\] /,'') }
+    end
   end
 end
